3116a07f08307b46fe3493fb61d019ca43b13033
Davide Conti <davide.conti@modernmt.eu>
2021-05-14 11:19:22 +0200

    Update version to 1.0.4

--- src/modernmt.ts
+++ src/modernmt.ts
@@ -8,7 +8,7 @@ export class ModernMT {
     private readonly http: HttpClient;
     public readonly memories: MemoryServices;
 
-    constructor(apiKey: string, platform = "modernmt-node", platformVersion = "1.0.3") {
+    constructor(apiKey: string, platform = "modernmt-node", platformVersion = "1.0.4") {
         const headers: any = {
             "MMT-ApiKey": apiKey,
             "MMT-Platform": platform,
